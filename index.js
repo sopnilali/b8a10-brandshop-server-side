0ccb53677f2a3313822813e2c49716ea313d12a0
csesopnil <sopnilstar@gmail.com>
2023-10-21 20:53:06 +0600

    added

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const corsConfig = {
   origin:'*',
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
+  headers: ["X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"]
 }
 // middlewars
 app.use(cors(corsConfig));
